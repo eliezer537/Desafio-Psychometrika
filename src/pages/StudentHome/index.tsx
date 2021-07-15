52aa1ae17487280be76129bcf214b8b0c831786e
Eliezer <eliezerlincon537@hotmail.com>
2021-07-15 00:33:55 -0300

    Admin Page and Student Page receiving GRADE array as property

--- src/pages/StudentHome/index.tsx
+++ src/pages/StudentHome/index.tsx
@@ -1,17 +1,58 @@
+import { useState, useEffect } from 'react';
+import { database } from '../../services/firebase';
+import { useParams } from 'react-router-dom';
+
 import { Header } from '../../components/Header';
 import { SubHeader } from '../../components/SubHeader';
 import { Grade } from '../../components/Grade';
 
+import { TransformGradeObjectIntoJSON } from '../../utils/TransformGradeObjectIntoJSON';
+
 import './styles.scss';
 
+type paramsType = {
+	schoolId: string;
+};
+
+type SchoolTypes = Array<{
+	id: string;
+	gradleName: string;
+	fronts: {
+		id: string;
+		frontName: string;
+		chapters: {
+			title: string;
+			url: string;
+		}[];
+	}[];
+}>;
+
 export function StudentHome() {
+	const { schoolId } = useParams<paramsType>();
+	const [schoolName, setSchoolName] = useState('');
+	const [gradeArray, setGradeArray] = useState<SchoolTypes>();
+
+	useEffect(() => {
+		const schoolRef = database.ref(`schools/${schoolId}`);
+
+		schoolRef.once('value', school => {
+			const databaseSchool = school.val();
+			setSchoolName(school.val().name);
+
+			TransformGradeObjectIntoJSON(databaseSchool);
+
+			setGradeArray(TransformGradeObjectIntoJSON(databaseSchool));
+		});
+	}, [schoolId]);
 	return (
 		<div className='student-container'>
 			<Header initialLetter='A' />
-			<SubHeader title='Nome da Escola' />
+			<SubHeader title={`${schoolName}`} />
 
 			<main>
-				<Grade />
+				{gradeArray?.map(gradle => (
+					<Grade key={gradle.id} grades={gradeArray} />
+				))}
 			</main>
 		</div>
 	);
